786b59efee96a6cd74acb95744e3950a73457de5
Ayush Shukla <126188592+ayush-shukla4@users.noreply.github.com>
2024-04-13 16:11:51 +0530

    Done with mongo db assignment

--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,10 +1,24 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin } = require("../db");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
+    const username = req.headers.username;
+    const password = req.headers.password;
+
+    await Admin.create({
+        username: username,
+        password: password
+    })
+    
+    res.json({
+          message: "Admin created successfully"
+    })
+   
+
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
